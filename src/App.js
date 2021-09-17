5765063906a4f20e37b15926df6c8ebcdede4d88
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-09-17 13:42:59 +0100

    Mock Image api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import images from './api-mock.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+const [ imageList, setImageList ] = useState(images.resources)
+    return (
+        <div className="image_grid">
+          {imageList.map((image) => <img src={image.url} alt={image.public_id}></img>)}
+        </div>
+    )
 }
 
-export default App;
+export default App
