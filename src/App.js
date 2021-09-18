b3d29e0092317c23174922a0e606349f75f6e985
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-09-18 12:59:21 +0100

    Load More Button

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import { getImages } from './api.js';
 import './App.css';
 
 const App = () => {
-const [ imageList, setImageList ] = useState([])
+  //api
+const [ imageList, setImageList ] = useState([]);
+//Load more button
+const [ nextCursor, setNextCursor ] = useState(null);
 
   // call api immediately
     useEffect(() => {
         const fetchData = async() => {
           const responseJSON = await getImages();
           setImageList(responseJSON.resources)
+          setNextCursor(responseJSON.next_cursor)
         };
 
         fetchData();
     }, []);
 
+    const handleLoadMoreButtonClick = async() => {
+      const responseJSON = await getImages(nextCursor)
+      setImageList((currentImageList) => [
+        ...currentImageList,
+        ...responseJSON.resources])
+        setNextCursor(responseJSON.next_cursor)
+    }
+
     return (
+      <>
         <div className="image_grid">
           {imageList.map((image) => (
             <img src={image.url} alt={image.public_id}></img>
           ))}
         </div>
+
+        <div className='footer'>
+          {nextCursor && <button onClick={handleLoadMoreButtonClick}>Load More</button>}
+        </div>
+      </>
     )
 }
 
--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 const API_URL = process.env.REACT_APP_API_URL
 
 //connect react frontend to backend
-export const getImages = async () => {
-  const response = await fetch(`${API_URL}/photos`);
+export const getImages = async (nextCursor) => {
+  //search (loadmore)
+const params = new URLSearchParams();
+
+if (nextCursor) {
+  params.append('next_cursor', nextCursor)
+}
+
+  //api call
+  const response = await fetch(`${API_URL}/photos?${params}`);
   const responseJSON = await response.json();
 
   return responseJSON
