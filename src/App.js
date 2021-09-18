913323ee4e7294c1290863e03a28aa206ec9052b
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-09-18 15:00:17 +0100

    search button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getImages } from './api.js';
+import { getImages, searchImages } from './api.js';
 import './App.css';
 
 const App = () => {
@@ -7,6 +7,8 @@ const App = () => {
 const [ imageList, setImageList ] = useState([]);
 //Load more button
 const [ nextCursor, setNextCursor ] = useState(null);
+//Search more button
+const [searchValue, setSearchValue] = useState('')
 
   // call api immediately
     useEffect(() => {
@@ -19,6 +21,7 @@ const [ nextCursor, setNextCursor ] = useState(null);
         fetchData();
     }, []);
 
+//Load more button
     const handleLoadMoreButtonClick = async() => {
       const responseJSON = await getImages(nextCursor)
       setImageList((currentImageList) => [
@@ -27,8 +30,36 @@ const [ nextCursor, setNextCursor ] = useState(null);
         setNextCursor(responseJSON.next_cursor)
     }
 
+    //search button
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const responseJSON = await searchImages(searchValue, nextCursor);
+    setImageList(responseJSON.resources);
+    setNextCursor(responseJSON.next_cursor);
+  };
+
+  // clear button
+  const resetForm = async () => {
+    const responseJSON = await getImages();
+    setImageList(responseJSON.resources);
+    setNextCursor(responseJSON.next_cursor);
+
+    setSearchValue('');
+  }
+
+
+
     return (
       <>
+        <form onSubmit={handleFormSubmit}>
+          <input value={searchValue}
+            onChange={(event)=> setSearchValue(event.target.value) }
+            required="required"
+            placeholder="Enter a search value..."></input>
+          <button type="submit">Search</button>
+          <button type="button"onClick={resetForm}>Clear</button>
+        </form>
         <div className="image_grid">
           {imageList.map((image) => (
             <img src={image.url} alt={image.public_id}></img>
--- src/api.js
+++ src/api.js
@@ -13,5 +13,19 @@ if (nextCursor) {
   const response = await fetch(`${API_URL}/photos?${params}`);
   const responseJSON = await response.json();
 
-  return responseJSON
+  return responseJSON;
+}
+
+export const searchImages = async(searchValue, nextCursor) => {
+  const params = new URLSearchParams();
+  params.append(`expression`, searchValue);
+
+  if(nextCursor) {
+    params.append('next_cursor', nextCursor)
+  }
+
+  const response = await fetch(`${API_URL}/search?${params}`);
+  const responseJSON = await response.json();
+
+  return responseJSON;
 }
