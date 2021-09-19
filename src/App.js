7d67c659b4bfd64a3203c42b4e32fc2d5461ff4e
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-09-19 17:23:10 +0100

    css grid edition

--- src/App.js
+++ src/App.js
@@ -4,33 +4,33 @@ import './App.css';
 
 const App = () => {
   //api
-const [ imageList, setImageList ] = useState([]);
-//Load more button
-const [ nextCursor, setNextCursor ] = useState(null);
-//Search more button
-const [searchValue, setSearchValue] = useState('')
+  const [imageList, setImageList] = useState([]);
+  //Load more button
+  const [nextCursor, setNextCursor] = useState(null);
+  //Search more button
+  const [searchValue, setSearchValue] = useState('')
 
   // call api immediately
-    useEffect(() => {
-        const fetchData = async() => {
-          const responseJSON = await getImages();
-          setImageList(responseJSON.resources)
-          setNextCursor(responseJSON.next_cursor)
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      const responseJSON = await getImages();
+      setImageList(responseJSON.resources)
+      setNextCursor(responseJSON.next_cursor)
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-//Load more button
-    const handleLoadMoreButtonClick = async() => {
-      const responseJSON = await getImages(nextCursor)
-      setImageList((currentImageList) => [
-        ...currentImageList,
-        ...responseJSON.resources])
-        setNextCursor(responseJSON.next_cursor)
-    }
+  //Load more button
+  const handleLoadMoreButtonClick = async () => {
+    const responseJSON = await getImages(nextCursor)
+    setImageList((currentImageList) => [
+      ...currentImageList,
+      ...responseJSON.resources])
+    setNextCursor(responseJSON.next_cursor)
+  }
 
-    //search button
+  //search button
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -50,27 +50,38 @@ const [searchValue, setSearchValue] = useState('')
 
 
 
-    return (
-      <>
-        <form onSubmit={handleFormSubmit}>
-          <input value={searchValue}
-            onChange={(event)=> setSearchValue(event.target.value) }
-            required="required"
-            placeholder="Enter a search value..."></input>
-          <button type="submit">Search</button>
-          <button type="button"onClick={resetForm}>Clear</button>
-        </form>
-        <div className="image_grid">
-          {imageList.map((image) => (
-            <img src={image.url} alt={image.public_id}></img>
-          ))}
+  return (
+    <>
+      <div className="App">
+        <h1>Welcome To Cloudinary Image Gallery</h1>
+        <div className="wrap">
+          <div className="search">
+            <form onSubmit={handleFormSubmit}>
+              <input value={searchValue}
+                onChange={(event) => setSearchValue(event.target.value)}
+                required="required"
+                className="searchTerm"
+                placeholder="Enter a search value..."></input>
+              <button type="submit" className="searchButton">Search</button>
+              <button type="button" onClick={resetForm} className="searchButton">Clear</button>
+            </form>
+          </div>
         </div>
+        <div className="container">
+          <div className="image_grid">
+            {imageList.map((image) => (
+              <img src={image.url} alt={image.public_id} className="img" />
+            ))}
+          </div>
+        </div>
+
 
         <div className='footer'>
           {nextCursor && <button onClick={handleLoadMoreButtonClick}>Load More</button>}
         </div>
-      </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default App
